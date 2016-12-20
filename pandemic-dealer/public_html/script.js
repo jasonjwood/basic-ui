b6fb94726b75c7ccf60c271ca0aa25aeab7b9c0d
Jason Wood <jason.j.wood@gmail.com>
2016-12-19 22:22:10 -0500

    shuffle + tests

--- pandemic-dealer/public_html/script.js
+++ pandemic-dealer/public_html/script.js
@@ -12,34 +12,44 @@ class Pile {
         this.cards.push(card);
     }
     
-    remove_from_top(card) {
-        this.cards.splice(this.cards.length-1, 1);
-    }
-    
     peek_at_top() {
         if (this.cards.length === 0) {
             return null;
         }
         return this.cards[this.cards.length-1];
     }
+    
+    remove_from_top() {
+        var card = this.peek_at_top();
+        this.cards.splice(this.cards.length-1, 1);
+        return card;
+    }
+    
+    
 }
 
 
 class Deck {
     constructor () {
         this.cards = ["a", "b", "c"];
+        this.piles = {};
     }
     
-    cards() {
+    cards () {
         return this.cards;
     }
     
-    draw_top() {
-        return this.cards[0];        
+    swap(l, x, y) {
+        var b = l[x];
+        l[x] = l[y];
+        l[y] = b;
     }
     
     shuffle() {
-        
+        for (var i = this.cards.length - 1; i>0; i--) {
+            var n = Math.floor(Math.random() * (i+1));
+            this.swap(this.cards, i, n);
+        }
     }
 }
 	
--- pandemic-dealer/test/unit/test-script.js
+++ pandemic-dealer/test/unit/test-script.js
@@ -1,8 +1,27 @@
 describe("Deck", function() {
-    it("returns the top card", function() {
+    it("can expose its cards", function() {
         var deck = new Deck();
-        var top = deck.draw_top();
-        expect(top=='a');
+        expect(deck.cards[0]).toEqual("a");
+        expect(deck.cards[1]).toEqual("b");
+        expect(deck.cards[2]).toEqual("c");
+    });
+    
+    
+    it("can be shuffled", function() {
+        var deck = new Deck();
+        var orderings = {};
+       
+        for (var i=0; i<5000; i++) {
+            deck.shuffle();
+            var order = deck.cards[0] + deck.cards[1] + deck.cards[2];
+            if (!orderings[order]) {
+                orderings[order] = 1;
+            } else {
+                orderings[order] = orderings[order] + 1;
+            }
+        }
+        
+        expect(orderings["abc"]>400).toEqual(true);
     });
 });
 
@@ -16,12 +35,19 @@ describe("Pile", function() {
         pile.add_to_top("TOP");
         expect(pile.peek_at_top()).toEqual("TOP");
     });
-    
     it("adds newest items to top", function() {
         var pile = new Pile();
         pile.add_to_top("a");
         pile.add_to_top("b");
         expect(pile.peek_at_top()).toEqual("b");        
+    });
+    it("removes cards from the top", function() {
+        var pile = new Pile();
+        pile.add_to_top("a");
+        pile.add_to_top("b");
+        expect(pile.remove_from_top()).toEqual("b");
+        expect(pile.remove_from_top()).toEqual("a");
+        expect(pile.remove_from_top()).toBeNull();
     });
 });
 
